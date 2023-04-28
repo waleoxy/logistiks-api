c50612824abd9aaaa95bfae9f4356d4ef715f644
Olawale Otubu <waleoxy@gmail.com>
2023-04-28 15:34:50 +0100

    feat: repository setup

--- src/user/services/user/user.service.ts
+++ src/user/services/user/user.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import User from 'src/entities/User.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
+
+  findAllUsers() {}
+
+  createUser() {}
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './controllers/user/user.controller';
 import { UserService } from './services/user/user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import User from 'src/entities/User.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
 })
-export class UserModule { }
+export class UserModule {}
